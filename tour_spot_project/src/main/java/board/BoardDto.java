3f13bb8ed0e1a7c94c7b38ea948ef5f763ae61b9
EZEN <EZEN@DESKTOP-UG9BBKL>
2022-11-02 13:11:46 +0900

    BoardDto & BoardDao 작업 2022/11/02 13:11

--- tour_spot_project/src/main/java/board/BoardDto.java
+++ tour_spot_project/src/main/java/board/BoardDto.java
@@ -5,92 +5,104 @@ import java.sql.Timestamp;
 public class BoardDto {
 	
 	private int no;				// PK
-	private int viewCnt;
-	private String title;
-	private String content;
+	private int viewCnt;		
+	private String title;		
+	private String content;		
 	private String id;			// FK
-	private String code;		// FK
-	private Timestamp regDate;
-	private Timestamp modDate;
+	private String map_url;		// FK
+	private Timestamp regDate;	
+	private Timestamp modDate;	
 	
-	public BoardDto(int no, int viewCnt, String title, String content, String id, String code, Timestamp regDate, Timestamp modDate) {
+	public BoardDto(int no, int viewCnt, String title, String content, String id, String map_url, Timestamp regDate,
+			Timestamp modDate) {
 		super();
 		this.no = no;
 		this.viewCnt = viewCnt;
 		this.title = title;
 		this.content = content;
 		this.id = id;
-		this.code = code;
+		this.map_url = map_url;
 		this.regDate = regDate;
 		this.modDate = modDate;
 	}
-	
+
+	public BoardDto(int no, int viewCnt, String title, String content, String id, String map_url, Timestamp regDate) {
+		super();
+		this.no = no;
+		this.viewCnt = viewCnt;
+		this.title = title;
+		this.content = content;
+		this.id = id;
+		this.map_url = map_url;
+		this.regDate = regDate;
+	}
+
 	public BoardDto(int no) {
 		super();
 		this.no = no;
 	}
-	
+
 	public int getNo() {
 		return no;
 	}
-	
+
 //	public void setNo(int no) {
 //		this.no = no;
 //	}
-	
+
 	public int getViewCnt() {
 		return viewCnt;
 	}
-	
+
 	public void setViewCnt(int viewCnt) {
 		this.viewCnt = viewCnt;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public String getContent() {
 		return content;
 	}
-	
+
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
-	public String getCode() {
-		return code;
+
+	public String getMap_url() {
+		return map_url;
 	}
-	
-	public void setCode(String code) {
-		this.code = code;
+
+	public void setMap_url(String map_url) {
+		this.map_url = map_url;
 	}
-	
+
 	public Timestamp getRegDate() {
 		return regDate;
 	}
-	
+
 	public void setRegDate(Timestamp regDate) {
 		this.regDate = regDate;
 	}
-	
+
 	public Timestamp getModDate() {
 		return modDate;
 	}
-	
-	public void setModDate(Timestamp modDate) {
-		this.modDate = modDate;
-	}
+
+//	public void setModDate(Timestamp modDate) {
+//		this.modDate = modDate;
+//	}
 }
